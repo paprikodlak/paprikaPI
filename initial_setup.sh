e67a609f9f454954fe6de104876403f06234dac2
paprikodlak <vit@pawlik.in>
2016-06-20 19:43:51 +0200

    Create initial_setup.sh

---
+++ initial_setup.sh
@@ -0,0 +1,18 @@
+#
+#
+#
+#
+
+sudo raspi-config
+
+#byobu hotkeys:
+#kill window: ctr+a, k
+sudo apt-get -y install byobu
+sudo byobu-enable
+
+
+
+#knowledge base:
+#
+#Nano:
+#jump to end of file:  ctrl + w + v
