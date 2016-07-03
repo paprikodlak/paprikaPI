d230d0cfd646aa4a7dd32c682172849c296cb0a5
Vít <vit@pawlik.in>
2016-07-03 18:12:43 +0000

    temp_logger.sh tinkering - still not working.

--- temp_logger.sh
+++ temp_logger.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env python
 
 ##info http://blog.scphillips.com/posts/2013/07/getting-a-python-script-to-run-in-the-background-as-a-service-on-boot/
 
@@ -12,22 +12,24 @@
 # Description:       Put a long description of the service here
 ### END INIT INFO
 
+# Using the lsb functions to perform the operations.
+#. /lib/lsb/init-functions
+
 # Change the next 3 lines to suit where you install your script and what you want to call it
-DIR=~/paprikaPI
-DAEMON=$DIR/temp_logger.py
-DAEMON_NAME=temp_logger
+DIR="/home/pi/paprikaPI"
+DAEMON="$DIR/temp_logger.py"
+DAEMON_NAME="temp_logger"
 
 # Add any command line options for your daemon here
 DAEMON_OPTS=""
 
 # This next line determines what user the script runs as.
 # Root generally not recommended but necessary if you are using the Raspberry Pi GPIO from Python.
-DAEMON_USER=pi
+DAEMON_USER="root"
 
 # The process ID of the script when it runs is stored here:
-PIDFILE=/var/run/$DAEMON_NAME.pid
+PIDFILE="/var/run/$DAEMON_NAME.pid"
 
-. /lib/lsb/init-functions
 
 do_start () {
     log_daemon_msg "Starting system $DAEMON_NAME daemon"
