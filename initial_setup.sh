6f9007c19844fd299749976dcd6dcd1590634dc4
paprikodlak <vit@pawlik.in>
2016-07-03 19:04:13 +0200

    intial_setup.sh, scroll mode hotkey for bylobu added

--- initial_setup.sh
+++ initial_setup.sh
@@ -11,7 +11,8 @@ sudo chmod +x raspi-wifi-shellscript.sh
 sudo ./raspi-wifi-shellscript.sh
 
 #byobu hotkeys:
-#kill window: ctr+a, k
+#kill window: ctr+a, k¨
+#scroll mode: F7
 echo -n
 echo -n "Installing byobu"
 sudo apt-get -y install byobu
